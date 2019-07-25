09ed1a292ecac82788883f6d13a72bfdb77a857a
michaelyohannes123 <miketj2019@gmail.com>
2019-07-24 21:16:50 -0700

    files

---
+++ main.py
@@ -0,0 +1,16 @@
+import webapp2
+import jinja2
+import os
+
+jinja_env=jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    undefined=jinja2.StrictUndefined, #catches template errors
+    autoescape=True
+)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("hello")
+
+app=webapp2.WSGIApplication([('/', MainPage), debug=True])
